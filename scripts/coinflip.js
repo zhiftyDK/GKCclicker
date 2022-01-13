46aeb091a823b5dc1fdd250a89068fd1d0400fc4
CodeZhifty <juulstausholmoscar@gmail.com>
2022-01-14 00:15:18 +0100

    Fixed bugs and error messages

--- scripts/coinflip.js
+++ scripts/coinflip.js
@@ -14,75 +14,102 @@ document.getElementById("coinflipAmount").addEventListener("keyup", function(eve
 });
 
 function flipCoin() {
-    firebase.database().ref("GKCscoreboard/" + displayName).get().then(function(GKCscoreboard_object){
-        //Check if you have enough money to coinflip
-        const gkcCheck = GKCscoreboard_object.val().GKC
-        const amountBetCheck = parseInt(document.getElementById("coinflipAmount").value);
-        if(gkcCheck >= amountBetCheck) {
-            //If you have enough then flip
-            document.getElementById("greenCoinflip").style.display = "none";
-            document.getElementById("redCoinflip").style.display = "none";
-            document.getElementById("defaultCoinflip").style.display = "none";
-            let randomInt = Math.floor(Math.random() * 2);
-            if(randomInt == 0) {
-                document.getElementById("coinflipButton").disabled = true;
-                document.getElementById("greenCoinflip").style.display = "block";
-                document.getElementById("greenCoinflip").play();
-                document.getElementById("greenCoinflip").onended = function() {
-                    document.getElementById("coinflipButton").disabled = false;
-                    const colorBet = document.getElementById("coinflipSelect").value;
-                    const amountBet = parseInt(document.getElementById("coinflipAmount").value);
-                    if(colorBet == "green") {
-                        firebase.database().ref("GKCscoreboard/" + displayName).get().then(function(GKCscoreboard_object){
-                            const GKC = GKCscoreboard_object.val().GKC
-                            firebase.database().ref("GKCscoreboard/" + displayName).update({
-                                GKC: GKC + amountBet
+    if(!displayName == ""){
+        firebase.database().ref("GKCscoreboard/" + displayName).get().then(function(GKCscoreboard_object){
+            //Check if you have enough money to coinflip
+            const gkcCheck = GKCscoreboard_object.val().GKC
+            const amountBetCheck = parseInt(document.getElementById("coinflipAmount").value);
+            if(document.getElementById("coinflipAmount").value == "") {
+                emptyFormAlert();
+                return;
+            }
+            if(gkcCheck >= amountBetCheck) {
+                //If you have enough then flip
+                document.getElementById("greenCoinflip").style.display = "none";
+                document.getElementById("redCoinflip").style.display = "none";
+                document.getElementById("defaultCoinflip").style.display = "none";
+                let randomInt = Math.floor(Math.random() * 2);
+                if(randomInt == 0) {
+                    document.getElementById("coinflipButton").disabled = true;
+                    document.getElementById("greenCoinflip").style.display = "block";
+                    document.getElementById("greenCoinflip").play();
+                    document.getElementById("greenCoinflip").onended = function() {
+                        document.getElementById("coinflipButton").disabled = false;
+                        const colorBet = document.getElementById("coinflipSelect").value;
+                        const amountBet = parseInt(document.getElementById("coinflipAmount").value);
+                        if(colorBet == "green") {
+                            firebase.database().ref("GKCscoreboard/" + displayName).get().then(function(GKCscoreboard_object){
+                                const GKC = GKCscoreboard_object.val().GKC
+                                firebase.database().ref("GKCscoreboard/" + displayName).update({
+                                    GKC: GKC + amountBet
+                                });
                             });
-                        });
-                        winningAlert(amountBet);
-                    } else {
-                        firebase.database().ref("GKCscoreboard/" + displayName).get().then(function(GKCscoreboard_object){
-                            const GKC = GKCscoreboard_object.val().GKC
-                            firebase.database().ref("GKCscoreboard/" + displayName).update({
-                                GKC: GKC - amountBet
+                            winningAlert(amountBet);
+                        } else {
+                            firebase.database().ref("GKCscoreboard/" + displayName).get().then(function(GKCscoreboard_object){
+                                const GKC = GKCscoreboard_object.val().GKC
+                                firebase.database().ref("GKCscoreboard/" + displayName).update({
+                                    GKC: GKC - amountBet
+                                });
                             });
-                        });
-                        losingAlert(amountBet);
+                            losingAlert(amountBet);
+                        }
                     }
                 }
-            }
-            else if(randomInt == 1) {
-                document.getElementById("coinflipButton").disabled = true;
-                document.getElementById("redCoinflip").style.display = "block";
-                document.getElementById("redCoinflip").play();
-                document.getElementById("redCoinflip").onended = function() {
-                    document.getElementById("coinflipButton").disabled = false;
-                    const colorBet = document.getElementById("coinflipSelect").value;
-                    const amountBet = parseInt(document.getElementById("coinflipAmount").value);
-                    if(colorBet == "red") {
-                        firebase.database().ref("GKCscoreboard/" + displayName).get().then(function(GKCscoreboard_object){
-                            const GKC = GKCscoreboard_object.val().GKC
-                            firebase.database().ref("GKCscoreboard/" + displayName).update({
-                                GKC: GKC + amountBet
+                else if(randomInt == 1) {
+                    document.getElementById("coinflipButton").disabled = true;
+                    document.getElementById("redCoinflip").style.display = "block";
+                    document.getElementById("redCoinflip").play();
+                    document.getElementById("redCoinflip").onended = function() {
+                        document.getElementById("coinflipButton").disabled = false;
+                        const colorBet = document.getElementById("coinflipSelect").value;
+                        const amountBet = parseInt(document.getElementById("coinflipAmount").value);
+                        if(colorBet == "red") {
+                            firebase.database().ref("GKCscoreboard/" + displayName).get().then(function(GKCscoreboard_object){
+                                const GKC = GKCscoreboard_object.val().GKC
+                                firebase.database().ref("GKCscoreboard/" + displayName).update({
+                                    GKC: GKC + amountBet
+                                });
                             });
-                        });
-                        winningAlert(amountBet);
-                    } else {
-                        firebase.database().ref("GKCscoreboard/" + displayName).get().then(function(GKCscoreboard_object){
-                            const GKC = GKCscoreboard_object.val().GKC
-                            firebase.database().ref("GKCscoreboard/" + displayName).update({
-                                GKC: GKC - amountBet
+                            winningAlert(amountBet);
+                        } else {
+                            firebase.database().ref("GKCscoreboard/" + displayName).get().then(function(GKCscoreboard_object){
+                                const GKC = GKCscoreboard_object.val().GKC
+                                firebase.database().ref("GKCscoreboard/" + displayName).update({
+                                    GKC: GKC - amountBet
+                                });
                             });
-                        });
-                        losingAlert(amountBet);
+                            losingAlert(amountBet);
+                        }
                     }
                 }
+            } else {
+                notEnoughAlert();
             }
-        } else {
-            notEnoughAlert();
-        }
-    });
+        });
+    } else {
+        notAuthAlert();
+    }
+}
 
+function notAuthAlert() {
+    if(document.getElementById("notAuthAlert")){
+        document.getElementById("notAuthAlert").remove();
+    }else if(document.getElementById("winningAlert")){
+        document.getElementById("winningAlert").remove();
+    } else if(document.getElementById("losingAlert")){
+        document.getElementById("losingAlert").remove();
+    } else if(document.getElementById("notEnoughAlert")){
+        document.getElementById("notEnoughAlert").remove();
+    }
+    const alert = document.createElement("div");
+    alert.classList = "alert alert-danger alert-dismissible fade show";
+    alert.id = "notAuthAlert";
+    alert.style.position = "absolute";
+    alert.style.right = "40px";
+    alert.style.bottom = "30px";
+    alert.innerHTML = `<strong>Error!</strong> You need to sign in to perform this action! <button type="button" class="btn-close" onclick="document.getElementById('notAuthAlert').remove()" aria-label="Close"></button>`
+    document.body.appendChild(alert);
 }
 
 function winningAlert(amountBet) {
@@ -92,6 +119,8 @@ function winningAlert(amountBet) {
         document.getElementById("losingAlert").remove();
     } else if(document.getElementById("notEnoughAlert")){
         document.getElementById("notEnoughAlert").remove();
+    } else if(document.getElementById("notAuthAlert")){
+        document.getElementById("notAuthAlert").remove();
     }
     const alert = document.createElement("div");
     alert.classList = "alert alert-success alert-dismissible fade show";
@@ -110,9 +139,11 @@ function losingAlert(amountBet) {
         document.getElementById("winningAlert").remove();
     } else if(document.getElementById("notEnoughAlert")){
         document.getElementById("notEnoughAlert").remove();
+    } else if(document.getElementById("notAuthAlert")){
+        document.getElementById("notAuthAlert").remove();
     }
     const alert = document.createElement("div");
-    alert.classList = "alert alert-danger alert-dismissible fade show";
+    alert.classList = "alert alert-warning alert-dismissible fade show";
     alert.id = "losingAlert";
     alert.style.position = "absolute";
     alert.style.right = "40px";
@@ -128,13 +159,37 @@ function notEnoughAlert() {
         document.getElementById("winningAlert").remove();
     } else if(document.getElementById("notEnoughAlert")){
         document.getElementById("notEnoughAlert").remove();
+    } else if(document.getElementById("notAuthAlert")){
+        document.getElementById("notAuthAlert").remove();
     }
     const alert = document.createElement("div");
-    alert.classList = "alert alert-warning alert-dismissible fade show";
+    alert.classList = "alert alert-danger alert-dismissible fade show";
     alert.id = "notEnoughAlert";
     alert.style.position = "absolute";
     alert.style.right = "40px";
     alert.style.bottom = "30px";
     alert.innerHTML = `<strong>Error!</strong> You don't have enough money to perform this action! <button type="button" class="btn-close" onclick="document.getElementById('notEnoughAlert').remove()" aria-label="Close"></button>`
+    document.body.appendChild(alert);
+}
+
+function emptyFormAlert() {
+    if(document.getElementById("emptyFormAlert")){
+        document.getElementById("emptyFormAlert").remove();
+    } else if(document.getElementById("losingAlert")){
+        document.getElementById("losingAlert").remove();
+    } else if(document.getElementById("winningAlert")){
+        document.getElementById("winningAlert").remove();
+    } else if(document.getElementById("notEnoughAlert")){
+        document.getElementById("notEnoughAlert").remove();
+    } else if(document.getElementById("notAuthAlert")){
+        document.getElementById("notAuthAlert").remove();
+    }
+    const alert = document.createElement("div");
+    alert.classList = "alert alert-danger alert-dismissible fade show";
+    alert.id = "emptyFormAlert";
+    alert.style.position = "absolute";
+    alert.style.right = "40px";
+    alert.style.bottom = "30px";
+    alert.innerHTML = `<strong>Error!</strong> You need to specify an amount! <button type="button" class="btn-close" onclick="document.getElementById('emptyFormAlert').remove()" aria-label="Close"></button>`
     document.body.appendChild(alert);
 }
--- scripts/pay.js
+++ scripts/pay.js
@@ -7,33 +7,55 @@ firebase.auth().onAuthStateChanged((user) => {
 });
 
 function pay() {
-    const friendInput = document.getElementById("friendInput");
-    const payAmount = document.getElementById("payAmount");
-
-    firebase.database().ref("GKCscoreboard/" + friendInput.value).get().then(function(GKCscoreboard_object){
-        if(GKCscoreboard_object.exists()){
-            const GKC = GKCscoreboard_object.val().GKC
-            firebase.database().ref("GKCscoreboard/" + friendInput.value).update({
-                GKC: GKC + parseInt(payAmount.value)
-            });
-            firebase.database().ref("GKCscoreboard/" + displayName).get().then(function(GKCscoreboard_object){
-                const GKC2 = GKCscoreboard_object.val().GKC
-                firebase.database().ref("GKCscoreboard/" + displayName).update({
-                    GKC: GKC2 - parseInt(payAmount.value)
+    if(!displayName == ""){
+        const friendInput = document.getElementById("friendInput");
+        const payAmount = document.getElementById("payAmount");
+    
+        firebase.database().ref("GKCscoreboard/" + friendInput.value).get().then(function(GKCscoreboard_object){
+            if(GKCscoreboard_object.exists()){
+                const GKC = GKCscoreboard_object.val().GKC
+                firebase.database().ref("GKCscoreboard/" + friendInput.value).update({
+                    GKC: GKC + parseInt(payAmount.value)
                 });
-            });
-        } else {
-            invalidPersonAlert();
-        }
-    });
+                firebase.database().ref("GKCscoreboard/" + displayName).get().then(function(GKCscoreboard_object2){
+                    const GKC2 = GKCscoreboard_object2.val().GKC
+                    firebase.database().ref("GKCscoreboard/" + displayName).update({
+                        GKC: GKC2 - parseInt(payAmount.value)
+                    });
+                });
+            } else {
+                invalidPersonAlert();
+            }
+        });
+    } else {
+        notAuthAlert();
+    }
+}
+
+function notAuthAlert() {
+    if(document.getElementById("notAuthAlert")){
+        document.getElementById("notAuthAlert").remove();
+    } else if(document.getElementById("invalidPersonAlert")){
+        document.getElementById("invalidPersonAlert").remove();
+    }
+    const alert = document.createElement("div");
+    alert.classList = "alert alert-danger alert-dismissible fade show";
+    alert.id = "notAuthAlert";
+    alert.style.position = "absolute";
+    alert.style.right = "40px";
+    alert.style.bottom = "30px";
+    alert.innerHTML = `<strong>Error!</strong> You need to sign in to perform this action! <button type="button" class="btn-close" onclick="document.getElementById('notAuthAlert').remove()" aria-label="Close"></button>`
+    document.body.appendChild(alert);
 }
 
 function invalidPersonAlert() {
     if(document.getElementById("invalidPersonAlert")){
         document.getElementById("invalidPersonAlert").remove();
+    } else if(document.getElementById("notAuthAlert")){
+        document.getElementById("notAuthAlert").remove();
     }
     const alert = document.createElement("div");
-    alert.classList = "alert alert-warning alert-dismissible fade show";
+    alert.classList = "alert alert-danger alert-dismissible fade show";
     alert.id = "invalidPersonAlert";
     alert.style.position = "absolute";
     alert.style.right = "40px";
