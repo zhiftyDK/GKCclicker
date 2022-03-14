6d1cd9eede74e57f7e4d8e9ac7382a9033ee86f5
CodeZhifty <juulstausholmoscar@gmail.com>
2022-03-14 14:46:14 +0100

    Dev of jackpot, still alot of errors :D

--- scripts/jackpot.js
+++ scripts/jackpot.js
@@ -31,7 +31,21 @@ function bet(){
                         document.getElementById("playerPicture").src = profilePicture;
                         document.getElementById("bettedAmount").innerText = `You have entered the poll with ${payAmountCheck} GKC!`
                         transferSuccessAlert(payAmountCheck);
-                        continueGame();
+                        firebase.database().ref("jackpot/").get().then(function(jackpot_object){
+                            if(jackpot_object.exists()){
+                                const keys = Object.keys(jackpot_object.val());
+                                let users = [];
+                                keys.map(username => {
+                                    users.push({...eval(`jackpot_object.val().${username}`), username});
+                                })
+                                console.log(users.length)
+                                if(users.length == 2){
+                                    firebase.database().ref("endTime/").update({
+                                        time: Date.now() + 60 * 1000
+                                    });
+                                }
+                            }
+                        });
                     } else {
                         notEnoughAlert();
                     }
@@ -52,33 +66,49 @@ firebase.database().ref("jackpot/").on("value", function(jackpot_object){
             users.push({...eval(`jackpot_object.val().${username}`), username});
         })
         users.sort(({betAmount:a}, {betAmount:b}) => b-a);
-
+        
         let betAmountSum = 0;
         users.forEach(element => {
             betAmountSum = betAmountSum + element.betAmount
         });
 
-        let userChance = [];
+        const timeLeft = document.getElementById("timeLeft");
+        if(users.length >= 2){
+            firebase.database().ref("endTime/").on("value", function(endTime_object){
+                if(endTime_object.exists()){
+                    const endTime = endTime_object.val().time;
+                    console.log(Date.now());
+                    console.log(endTime);
+                    setInterval(() => {
+                        let secondsLeft = ((endTime - Date.now()) / 1000).toFixed(0);
+                        if(secondsLeft > 0){
+                            timeLeft.innerText = secondsLeft;
+                        } else {
+                            firebase.database().ref("jackpot/").remove();
+                            document.getElementById("removeOnJoin").style.display = "block";
+                            document.getElementById("player").style.display = "none";
+                            findWinner(users);
+                        }
+                    }, 500);
+                }
+            });
+        } else {
+            timeLeft.innerText = "Waiting for players!"
+        }
         
         for (let i = 0; i < users.length; i++) {
             let username = users[i].username;
             let betAmount = users[i].betAmount;
             let picture = users[i].picture;
             let winChance = parseFloat(betAmount / betAmountSum * 100).toFixed(2);
-            
-            for (let i = 0; i < parseFloat(betAmount / betAmountSum * 100).toFixed(0); i++) {
-                userChance.push(username);
-            }
-            console.log(userChance);
 
             if(username == applicationName){
                 document.getElementById("removeOnJoin").style.display = "none";
-                document.getElementById("playerPicture").style.display = "block";
+                document.getElementById("player").style.display = "block";
                 document.getElementById("playerPicture").src = profilePicture;
                 document.getElementById("bettedAmount").innerText = `You have entered the poll with ${betAmount} GKC!`
                 document.getElementById("winningChance").innerText = `Your chance of winning is: ${winChance}%`
             } else {
-                console.log(users[i])
                 const div = document.createElement("div");
                 div.innerHTML = `
                 <div class="d-flex justify-content-center align-items-center m-2">
@@ -95,8 +125,36 @@ firebase.database().ref("jackpot/").on("value", function(jackpot_object){
     }
 });
 
-function findWinner(){
+function findWinner(entries){
+    console.log(entries)
+    const randomPercentage = Math.random() * 100;
+    let currentPercentage = 0;
+    let betAmountSum = 0;
+    entries.forEach(element => {
+        betAmountSum = betAmountSum + element.betAmount
+    });
 
+    for (let i = 0; i < entries.length; i++) {
+        let userPercentage = entries[i].betAmount / betAmountSum * 100;
+        currentPercentage += userPercentage;
+        if(currentPercentage > randomPercentage){
+            console.log(entries[i].username);
+            if(applicationName == entries[i].username){
+                youWin(betAmountSum);
+                firebase.database().ref("GKCscoreboard/" + applicationName).get().then(function(GKCscoreboard_object){
+                    if(GKCscoreboard_object.exists()){
+                        const GKC = GKCscoreboard_object.val().GKC
+                        firebase.database().ref("GKCscoreboard/" + entries[i].username).update({
+                            GKC: GKC + parseInt(betAmountSum)
+                        });
+                    }
+                });
+            } else {
+                youLose();
+                return;
+            }
+        }
+    }
 }
 
 function notAuthAlert() {
@@ -106,6 +164,10 @@ function notAuthAlert() {
         document.getElementById("notEnoughAlert").remove();
     } else if(document.getElementById("transferSuccessAlert")){
         document.getElementById("transferSuccessAlert").remove();
+    } else if(document.getElementById("victory")){
+        document.getElementById("victory").remove();
+    } else if(document.getElementById("lose")){
+        document.getElementById("lose").remove();
     }
     const alert = document.createElement("div");
     alert.classList = "alert alert-danger alert-dismissible fade show";
@@ -124,6 +186,10 @@ function notEnoughAlert() {
         document.getElementById("notAuthAlert").remove();
     } else if(document.getElementById("transferSuccessAlert")){
         document.getElementById("transferSuccessAlert").remove();
+    } else if(document.getElementById("victory")){
+        document.getElementById("victory").remove();
+    } else if(document.getElementById("lose")){
+        document.getElementById("lose").remove();
     }
     const alert = document.createElement("div");
     alert.classList = "alert alert-danger alert-dismissible fade show";
@@ -142,6 +208,10 @@ function transferSuccessAlert(amount) {
         document.getElementById("notAuthAlert").remove();
     } else if(document.getElementById("notEnoughAlert")){
         document.getElementById("notEnoughAlert").remove();
+    } else if(document.getElementById("victory")){
+        document.getElementById("victory").remove();
+    } else if(document.getElementById("lose")){
+        document.getElementById("lose").remove();
     }
     const alert = document.createElement("div");
     alert.classList = "alert alert-success alert-dismissible fade show";
@@ -150,5 +220,49 @@ function transferSuccessAlert(amount) {
     alert.style.right = "40px";
     alert.style.bottom = "30px";
     alert.innerHTML = `<strong>Success!</strong> You have entered the jackpot poll with ${amount} GKC! <button type="button" class="btn-close" onclick="document.getElementById('transferSuccessAlert').remove()" aria-label="Close"></button>`
+    document.body.appendChild(alert);
+}
+
+function youWin(amount) {
+    if(document.getElementById("victory")){
+        document.getElementById("victory").remove();
+    } else if(document.getElementById("notAuthAlert")){
+        document.getElementById("notAuthAlert").remove();
+    } else if(document.getElementById("notEnoughAlert")){
+        document.getElementById("notEnoughAlert").remove();
+    } else if(document.getElementById("transferSuccessAlert")){
+        document.getElementById("transferSuccessAlert").remove();
+    } else if(document.getElementById("lose")){
+        document.getElementById("lose").remove();
+    }
+    const alert = document.createElement("div");
+    alert.classList = "alert alert-success alert-dismissible fade show";
+    alert.id = "victory";
+    alert.style.position = "absolute";
+    alert.style.right = "40px";
+    alert.style.bottom = "30px";
+    alert.innerHTML = `<strong>Victory!</strong> You won ${amount} GKC! <button type="button" class="btn-close" onclick="document.getElementById('victory').remove()" aria-label="Close"></button>`
+    document.body.appendChild(alert);
+}
+
+function youLose() {
+    if(document.getElementById("lose")){
+        document.getElementById("lose").remove();
+    } else if(document.getElementById("notAuthAlert")){
+        document.getElementById("notAuthAlert").remove();
+    } else if(document.getElementById("notEnoughAlert")){
+        document.getElementById("notEnoughAlert").remove();
+    } else if(document.getElementById("transferSuccessAlert")){
+        document.getElementById("transferSuccessAlert").remove();
+    } else if(document.getElementById("victory")){
+        document.getElementById("victory").remove();
+    }
+    const alert = document.createElement("div");
+    alert.classList = "alert alert-warning alert-dismissible fade show";
+    alert.id = "lose";
+    alert.style.position = "absolute";
+    alert.style.right = "40px";
+    alert.style.bottom = "30px";
+    alert.innerHTML = `<strong>Unfortunately!</strong> You lost!<button type="button" class="btn-close" onclick="document.getElementById('lose').remove()" aria-label="Close"></button>`
     document.body.appendChild(alert);
 }
