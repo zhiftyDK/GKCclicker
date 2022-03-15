739649b60bfc3ba2eadd0b677a813a04cce6b9b5
CodeZhifty <juulstausholmoscar@gmail.com>
2022-03-15 09:39:47 +0100

    Fixed authentication

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -15,10 +15,10 @@ firebase.auth().onAuthStateChanged((user) => {
     if (user) {
         // User is signed in
         loadUserMenu(user.displayName, user.photoURL, user.email);
-        if(user.email.split("@").shift().replace(/./g, "").replace(/#/g, "").replace(/$/g, "").charAt(0) == /[0-9]/g){
-            var username = "user_" + user.email.split("@").shift();
+        if(user.email.split("@").shift().replace(/\./g,'').replace(/#/g, "").replace(/\$/g, "").replace(/[\[\]']+/g,'').charAt(0) == /[0-9]/g){
+            var username = "user_" + user.email.split("@").shift().replace(/\./g,'').replace(/#/g, "").replace(/\$/g, "").replace(/[\[\]']+/g,'');
         } else {
-            var username = user.email.split("@").shift().replace(/./g, "").replace(/#/g, "").replace(/$/g, "");
+            var username = user.email.split("@").shift().replace(/\./g,'').replace(/#/g, "").replace(/\$/g, "").replace(/[\[\]']+/g,'');
         }
         let email = user.email;
         let userid = user.uid;
