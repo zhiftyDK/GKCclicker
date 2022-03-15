c9bec978fe11d4b98be868767a311bbb37e4a938
CodeZhifty <juulstausholmoscar@gmail.com>
2022-03-15 14:12:58 +0100

    Discarding changes

--- scripts/jackpot.js
+++ scripts/jackpot.js
@@ -114,7 +114,6 @@ firebase.database().ref("jackpot/").on("value", function(jackpot_object){
                             firebase.database().ref("jackpot/").remove();
                             document.getElementById("removeOnJoin").style.display = "flex";
                             document.getElementById("player").style.display = "none";
-                            console.log("Finding winner!")
                             findWinner(users);
                         }
                     }, 500);
@@ -154,38 +153,35 @@ firebase.database().ref("jackpot/").on("value", function(jackpot_object){
 });
 
 function findWinner(entries){
-    if(entries != ""){
-        console.log(entries)
-        const randomPercentage = Math.random() * 100;
-        let currentPercentage = 0;
-        let betAmountSum = 0;
-        entries.forEach(element => {
-            betAmountSum = betAmountSum + element.betAmount
-        });
-    
-        for (let i = 0; i < entries.length; i++) {
-            let userPercentage = entries[i].betAmount / betAmountSum * 100;
-            currentPercentage += userPercentage;
-            if(currentPercentage > randomPercentage){
-                console.log(entries[i].username);
-                if(applicationName == entries[i].username){
-                    youWin(betAmountSum);
-                    firebase.database().ref("GKCscoreboard/" + applicationName).get().then(function(GKCscoreboard_object){
-                        if(GKCscoreboard_object.exists()){
-                            const GKC = GKCscoreboard_object.val().GKC
-                            firebase.database().ref("GKCscoreboard/" + entries[i].username).update({
-                                GKC: GKC + parseInt(betAmountSum)
-                            });
-                        }
-                    });
-                    return;
-                } else {
-                    youLose();
-                    return;
-                }
+    console.log(entries)
+    const randomPercentage = Math.random() * 100;
+    let currentPercentage = 0;
+    let betAmountSum = 0;
+    entries.forEach(element => {
+        betAmountSum = betAmountSum + element.betAmount
+    });
+
+    for (let i = 0; i < entries.length; i++) {
+        let userPercentage = entries[i].betAmount / betAmountSum * 100;
+        currentPercentage += userPercentage;
+        if(currentPercentage > randomPercentage){
+            console.log(entries[i].username);
+            if(applicationName == entries[i].username){
+                youWin(betAmountSum);
+                firebase.database().ref("GKCscoreboard/" + applicationName).get().then(function(GKCscoreboard_object){
+                    if(GKCscoreboard_object.exists()){
+                        const GKC = GKCscoreboard_object.val().GKC
+                        firebase.database().ref("GKCscoreboard/" + entries[i].username).update({
+                            GKC: GKC + parseInt(betAmountSum)
+                        });
+                    }
+                });
+                return;
+            } else {
+                youLose();
+                return;
             }
         }
-        entries = "";
     }
 }
 
